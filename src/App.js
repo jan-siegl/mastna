ca95c5c16fe6dc4c4b83c6a3cff2b3adb76efd95
Jan Siegl <42580612+jan-siegl@users.noreply.github.com>
2021-04-18 01:56:10 +0200

    hero

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.scss';
 import Form from "./form/Form";
 import Eliska from "./img/eliska.png"
 import Adam from "./img/adam.png"
+import Kolaz from "./img/kolaz.png"
 
 function App() {
     return (
         <div className="App">
-            <Form/>
+            {/*<Form/>*/}
+            <div className="hero">
+                <h1>Adam Slaměna & Eliška Machátová</h1>
+                <img src={Kolaz} alt="Adam a Eliska"/>
+                <p>24/7/2021 13:00</p>
+                <a href="#">dejte nám vědět, že dorazíte</a>
+            </div>
         </div>
     );
 }
